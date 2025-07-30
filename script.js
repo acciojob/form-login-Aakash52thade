a66fb40ac03e71b5da9c51bf83041a068eefa74c
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-07-30 11:04:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const form = document.getElementById("form1");
+	const fname = form["fname"].value;
+	const lname = form["lname"].value;
+
+	alert(fname+" "+lname);
+	return;
 
 }
